19bbc4d9a8f71d6abfbe7f395ada845bf53d8909
Christian Rudder <ringthebellpepper@gmail.com>
2023-01-20 11:28:57 -0500

    trying to fix the hot mess that is the bottom of the page (very unresponsive trying more duct tape and glue)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,13 @@ import './Footer.css'
 function Footer () {
     return (
         <div className='footer'>
-            <a href='https://opencollective.com/yt-focus'> Donate</a>
-            <Link to="/privacy">Privacy</Link>
-            <Link to="/help">Help</Link>
-            <a href='https://github.com/yt-focus/yt-focus'> Github</a>
+            <div className='footer-links'>
+                <a href='https://opencollective.com/yt-focus'> Donate</a>
+                <Link to="/privacy">Privacy</Link>
+                <Link to="/help">Help</Link>
+                <a href='https://github.com/yt-focus/yt-focus'> Github</a>
+
+            </div>
             <p className='copyright'>Copyright © {new Date().getFullYear()} YT Focus. All rights reserved.</p>
         </div>  
     );
